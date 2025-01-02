952d7236fe958a78db1a407c54e0f97256a3e293
SazakiMaeda <sazakimaeda@gmail.com>
2025-01-02 17:06:53 +0800

    +chatik

---
+++ src/com/sazakimaeda/http/socket/SocketRunner.java
@@ -0,0 +1,25 @@
+package com.sazakimaeda.http.socket;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketRunner {
+    public static void main(String[] args) throws IOException {
+        // http - 80
+        // https - 443
+        InetAddress inetAddress = InetAddress.getByName("localhost");
+        try (Socket socket = new Socket(inetAddress, 7777);
+             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)
+        ) {
+            while (scanner.hasNextLine()) {
+                String request = scanner.nextLine();
+                outputStream.writeUTF(request);
+                System.out.println("Response: " + inputStream.readUTF());
+            }
+        }
+    }
+}
---
+++ src/com/sazakimaeda/http/socket/SocketServerRunner.java
@@ -0,0 +1,28 @@
+package com.sazakimaeda.http.socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketServerRunner {
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(7777);
+             Socket socket = serverSocket.accept();
+             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+             DataInputStream in = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)
+        ){
+            String request = in.readUTF();
+            while (!"stop".equals(request)) {
+                System.out.println("Client request: " + request);
+                String response = scanner.nextLine();
+                out.writeUTF(response);
+                request = in.readUTF();
+            }
+        }
+    }
+}
